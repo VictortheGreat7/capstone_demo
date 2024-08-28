c51a497174f729ef454c46c071697f1680f73996
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2024-08-28 14:17:10 +0100

    Eighth Commit

--- capstone_terraform/main.tf
+++ capstone_terraform/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "capstone" {
   }
 
   azure_active_directory_role_based_access_control {
-    # managed                = true
+    managed                = true
     azure_rbac_enabled     = true
     admin_group_object_ids = [azuread_group.aks_admins.id]
   }
